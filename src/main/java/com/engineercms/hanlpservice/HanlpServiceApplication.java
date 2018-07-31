f719e995429489ad8a73f2e8f779b3a3d3be8515
EricChen <qiang.chen04@hand-china.com>
2018-07-31 20:45:01 +0800

    集成Hanlp

--- src/main/java/com/engineercms/hanlpservice/HanlpServiceApplication.java
+++ src/main/java/com/engineercms/hanlpservice/HanlpServiceApplication.java
@@ -3,9 +3,11 @@ package com.engineercms.hanlpservice;
 import com.spring4all.swagger.EnableSwagger2Doc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @EnableSwagger2Doc
 @SpringBootApplication
+@PropertySource("classpath:hanlp.properties")
 public class HanlpServiceApplication {
 
 	public static void main(String[] args) {
