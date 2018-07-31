d583a9346a6c03ee8d6472acc745bbc1972677b6
EricChen <qiang.chen04@hand-china.com>
2018-07-31 13:38:30 +0800

    init commit

---
+++ src/main/java/com/engineercms/hanlpservice/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.engineercms.hanlpservice.controller;
+
+import com.engineercms.hanlpservice.dto.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author EricChen 2018-5-16
+ * @email qiang.chen04@hand-china.com
+ */
+@Api(tags="测试请求")
+@RestController
+public class UserController {
+
+    @ApiOperation("创建用户")
+    @PostMapping("/users")
+    public User create(@RequestBody @Valid User user) {
+        return user;
+    }
+
+    @ApiOperation("用户详情")
+    @GetMapping("/users/{id}")
+    public User findById(@PathVariable Long id) {
+        return new User("bbb", 21, "上海", "aaa@bbb.com");
+    }
+
+    @ApiOperation("用户列表")
+    @GetMapping("/users")
+    public List<User> list(@ApiParam("查看第几页") @RequestParam int pageIndex,
+                           @ApiParam("每页多少条") @RequestParam int pageSize) {
+        List<User> result = new ArrayList<>();
+        result.add(new User("aaa", 50, "北京", "aaa@ccc.com"));
+        result.add(new User("bbb", 21, "广州", "aaa@ddd.com"));
+        return result;
+    }
+
+    @ApiIgnore
+    @DeleteMapping("/users/{id}")
+    public String deleteById(@PathVariable Long id) {
+        return "delete user : " + id;
+    }
+}
---
+++ src/test/java/com/engineercms/hanlpservice/HanlpServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.engineercms.hanlpservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HanlpServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
